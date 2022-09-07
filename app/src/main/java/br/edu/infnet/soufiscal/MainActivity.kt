9977ce749b2b426a415ce89232596fd25ac86f34
Cahcastroc <camila.castronunes@gmail.com>
2022-09-07 19:05:30 -0300

    Validação campos EditText- Cadastro

--- app/src/main/java/br/edu/infnet/soufiscal/MainActivity.kt
+++ app/src/main/java/br/edu/infnet/soufiscal/MainActivity.kt
@@ -1,22 +1,25 @@
 package br.edu.infnet.soufiscal
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns.EMAIL_ADDRESS
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import java.lang.IllegalArgumentException
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appAuth: FirebaseAuth
-    private var appUser : FirebaseUser? = null
+    private var appUser: FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
         appAuth = FirebaseAuth.getInstance()
 
@@ -24,21 +27,25 @@ class MainActivity : AppCompatActivity() {
         val btCadastrar = findViewById<Button>(R.id.btCadastrar)
 
         btLogin.setOnClickListener {
-            try{
+            try {
                 val etEmail = findViewById<EditText>(R.id.etEmail)
                 val etSenha = findViewById<EditText>(R.id.etSenha)
 
                 appAuth
-                    .signInWithEmailAndPassword(etEmail.text.toString(),etSenha.text.toString())
+                    .signInWithEmailAndPassword(etEmail.text.toString(), etSenha.text.toString())
                     .addOnCompleteListener {
-                        if (it.isSuccessful){
+                        if (it.isSuccessful) {
                             appUser = appAuth.currentUser
-                        }else{
+                        } else {
                             appUser = null
                         }
                     }
-            } catch (ex: IllegalArgumentException){
-                Toast.makeText(this,"O campo de e-mail e senha não podem ser nulos.",Toast.LENGTH_LONG).show()
+            } catch (ex: IllegalArgumentException) {
+                Toast.makeText(
+                    this,
+                    "O campo de e-mail e senha não podem ser nulos.",
+                    Toast.LENGTH_LONG
+                ).show()
             }
 
         }
@@ -49,19 +56,36 @@ class MainActivity : AppCompatActivity() {
                 val etEmail = findViewById<EditText>(R.id.etEmail)
                 val etSenha = findViewById<EditText>(R.id.etSenha)
 
-                    appAuth.createUserWithEmailAndPassword(etEmail.text.toString(),etSenha.text.toString())
-                        .addOnCompleteListener {
-                            if(it.isSuccessful){
-                                appUser = appAuth.currentUser
-                            }
-                        }
+                if (validaFormatoEmail(etEmail) && validaFormatoSenha(etSenha)) {
 
-            } catch (ex: IllegalArgumentException){
-                Toast.makeText(this,"O campo de e-mail e senha não podem ser nulos.",Toast.LENGTH_LONG).show()
+                        appAuth.createUserWithEmailAndPassword(
+                            etEmail.text.toString(),
+                            etSenha.text.toString()
+                        )
+                            .addOnCompleteListener {
+                                if (it.isSuccessful) {
+                                    appUser = appAuth.currentUser
+                                    Toast.makeText(
+                                        this,
+                                        "Cadastro realizado com sucesso!",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                }
+
+                            }
+                    }
+
+            } catch (ex: IllegalArgumentException) {
+                Toast.makeText(
+                    this,
+                    "O campo de e-mail e senha não podem ser nulos.",
+                    Toast.LENGTH_LONG
+                ).show()
             }
 
         }
     }
+
     override fun onStart() {
         super.onStart()
         appUser = appAuth.currentUser
@@ -73,4 +97,27 @@ class MainActivity : AppCompatActivity() {
         appAuth.signOut()
     }
 
+
+    fun validaFormatoEmail(etEmail: EditText): Boolean {
+        if (EMAIL_ADDRESS.matcher(etEmail.text.toString()).matches()) {
+            return true
+        } else {
+            etEmail.error = "E-mail inválido"
+            return false
+        }
+    }
+
+
+    fun validaFormatoSenha(etSenha: EditText): Boolean {
+        if (etSenha.text.toString().length >= 6) {
+            return true
+        } else{
+            etSenha.error = "Mínimo de 6 caracteres"
+            return false
+        }
+    }
 }
+
+
+
+
